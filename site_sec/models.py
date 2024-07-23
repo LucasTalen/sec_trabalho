ed88a99a8c1bdc37c59530898e5920e61aa14150
LucasTalen <lucaspaula58227@outlook.com>
2024-07-23 17:08:42 -0300

    Criado banco de dados e tabela Funcionario

--- site_sec/models.py
+++ site_sec/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=50)
+    CPF = models.CharField(max_length=11, primary_key=True)
+    nascimento = models.DateField()
+    email = models.EmailField()
+    telefone = models.CharField(max_length=15)
+    cargo = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
