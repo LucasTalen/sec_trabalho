b79b6d07214d305f01b315b5093158082726bb45
LucasTalen <lucaspaula58227@outlook.com>
2024-08-01 10:35:22 -0300

    subindo arquivos

--- site_sec/urls.py
+++ site_sec/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('pesquisar/funcionario/', views.buscar_funcionario, name='pesquisar_funcionario'),
     path('cadastrar/funcionario/', views.cadastrar_funcionario, name='cadastrar_funcionario'),
     path('criar/funcionario/', views.criar_funcionario, name='criar_funcionario'),
+    path('editar/funcionario/', views.editar_funcionario, name='editar_funcionario'),
+    path('perfil/<str:cpf>/', views.perfil_funcionario, name='perfil_funcionario'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- site_sec/views.py
+++ site_sec/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Funcionario
@@ -47,3 +47,41 @@ def buscar_funcionario(request):
         resultado = [{'nome':funcionario.nome, 'cpf':funcionario.CPF} for funcionario in sugestao]
         return JsonResponse(resultado, safe=False)
     return JsonResponse({'error':'Request invalida'}, status=400)
+
+
+@csrf_exempt
+def perfil_funcionario(request, cpf):
+    funcionario = get_object_or_404(Funcionario, CPF=cpf)
+    print(funcionario.nascimento)
+    return render(request, 'site_sec/perfil.html', {'funcionario':funcionario})
+
+
+@csrf_exempt
+def editar_funcionario(request):
+    if request.method == 'POST':
+        nome = request.POST.get('full-name')
+        cpf = request.POST.get('cpf')
+        nascimento = request.POST.get('dob')
+        telefone = request.POST.get('phone')
+        cargo = request.POST.get('position')
+        email = request.POST.get('email')
+        foto_de_perfil = request.FILES.get('profile-pic')
+        
+        print(cpf)
+        print(nome)
+        
+        funcionario = get_object_or_404(Funcionario, CPF=cpf)
+        
+                
+        funcionario.nome=nome
+        
+        funcionario.CPF=cpf
+        funcionario.nascimento=nascimento
+        funcionario.email=email
+        funcionario.telefone=telefone
+        funcionario.cargo=cargo
+        if foto_de_perfil:
+            funcionario.foto_perfil=foto_de_perfil
+        funcionario.save()
+        
+        return render(request, f'site_sec/perfil.html')
